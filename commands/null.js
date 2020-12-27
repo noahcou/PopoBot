5d56cb67baba643ebc6399a978aecfbd51781643
Noah Cou <noahjcou@gmail.com>
2020-12-27 02:41:44 -0500

    added groundwork for command response

---
+++ commands/null.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    console.log("Someone tried to execute a command")
+}
--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-    console.log("pinged")
+    console.log("POPOPING")
     message.channel.send(`Pong! Latency is ${message.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`).catch(console.error);
 }
