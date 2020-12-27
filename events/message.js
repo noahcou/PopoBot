cc3cd10c4e4bededbf320746ff8f83a6327a950f
Noah Cou <noahjcou@gmail.com>
2020-12-27 11:18:28 -0500

    added basic responses and some work has been done on command responses, trying to work on roles

--- events/message.js
+++ events/message.js
@@ -3,23 +3,57 @@ module.exports = (client, message) => {
   if (message.author.bot) return;
 
   // check if they are doing a command
-  if (preftrue(client.config.prefix, message, true)) {
+  if (message.channel.id != "792622815466881024" && preftrue(client.config.prefix, message, true)) {
     pref = preftrue(client.config.prefix, message, false)
     const args = message.content.slice(pref.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     // Grab the command data from the client.commands Enmap
     const cmd = client.commands.get(command);
     // don't crash if don't exist
-    if (!cmd) return;
-    // Run the command
-    cmd.run(client, message, args);
-    console.log("asdf")
-    return;
+    if (!cmd) {
+      client.commands.get("null").run(client, message, args)
+    } else {
+      // Run the command
+      cmd.run(client, message, args);
+      console.log("asdf")
+      return;
+    }
+    
   }
 
   // check if message contains certain things before passing it forward (sorry stuff like that)
+  if (message.content.indexOf("sorry") != -1) {
+    console.log("They said sorry!")
+    message.channel.send("Damn right you are")
+    return;
+  }
+
+  if (message.content.indexOf("Popo") != -1 || message.content.indexOf("popo") != -1) {
+    console.log("They said Popo")
+    rand = Math.floor(Math.random() * 100)
+    chance = 35
+    if (rand < chance) {
+      message.channel.send("POOOOOOOOOOPOOOOOOOOOOO!!!")
+    }
+    return;
+  }
 
   // trigger random events
+  if (message.channel.id == "792293253512036355") {
+    rand = Math.floor(Math.random() * 100)
+    chance = 5
+    if (rand < chance) {
+      console.log("Telling them to shut up")
+      message.channel.send("Shut Up Maggot!")
+    }
+    return;
+  }
+
+  if ((message.author.id == "325769856047775755" || message.author.id == "353687311449325569") && message.content.indexOf("?") != -1) {
+    console.log("Woman asked question")
+    message.channel.send("Oh look, a woman who doesn't know any better. What are the odds?")
+  }
+
 
   //update status becasue it keeps turning off
   client.user.setActivity(`${client.config.prefix[0]} help`);
